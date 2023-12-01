12047eb4fb2ee7ae14703365f33ed52d35fb985b
s22025 <s22025@std.it-college.ac.jp>
2023-12-02 02:55:57 +0900

    Fragmentテスト5 現在地からもデータ取得できた

--- app/src/main/java/jp/ac/it_college/std/s22025/fragmentbasicaltest/SelectCityFragment.kt
+++ app/src/main/java/jp/ac/it_college/std/s22025/fragmentbasicaltest/SelectCityFragment.kt
@@ -1,16 +1,27 @@
 package jp.ac.it_college.std.s22025.fragmentbasicaltest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.UiThread
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
@@ -28,9 +39,9 @@ import jp.ac.it_college.std.s22025.fragmentbasicaltest.model.WeatherInfo
 private val ktorClient = HttpClient(CIO) {
     engine {
         endpoint {
-            connectTimeout = 5000
-            requestTimeout = 5000
-            socketTimeout = 5000
+            connectTimeout = 7000
+            requestTimeout = 7000
+            socketTimeout = 7000
         }
     }
     install(ContentNegotiation) {
@@ -46,14 +57,11 @@ private val ktorClient = HttpClient(CIO) {
 
 /** Fragment Result API を通じて Activity へデータを返すためのイベント名 */
 internal const val REQUEST_SELECTED_CITY = "selectedCity"
-
 /** Fragment Result API を通じて Activity へメニュー名を返すための識別子 */
 internal const val RESULT_NAME = "cityName"
-
 /** Fragment Result API を通じて Activity へデータを返すためのイベント名 */
 /* タブレットで用いる*/
 internal const val REQUEST_BACK_CITY = "backCity"
-
 
 internal const val GAINED_CITY_NAME = "gainedCityName"
 internal const val GAINED_CITY_DATETEXT = "gainedCityDateText"
@@ -84,10 +92,36 @@ class SelectCityFragment : Fragment() {
     // Bindingクラス->レイアウトファイルの要素にアクセスするための自動生成されたクラス
     private var _binding: FragmentSelectCityBinding? = null
 
-
     //Activityのときと同じようにbindingを使うための工夫
     private val binding get() = _binding!!
 
+    //位置情報を取得するためのライブラリ↓
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //位置情報の更新に関する設定情報が格納されている↓
+    private lateinit var locationRequest: LocationRequest
+
+    //位置情報が取得できた・変わった等位置情報に関するイベントが発生したときのリスナメソッドがで意義されている↓
+    private lateinit var locationCallback: LocationCallback
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val isGrantedFineLocation =
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)
+        val isGrantedCoarseLocation =
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false)
+        //どちらかの権限が許可もらえたという場合
+        if(isGrantedFineLocation || isGrantedCoarseLocation){
+            requestLocationUpdates()
+            return@registerForActivityResult
+        }
+        //結局権限の許可をもらえなかったとき とりあえずログだけだしとく
+        Log.w("(;_;)", "許可がもらえませんでした")
+    }
+
+    private var latitude = 0.0
+    private var longitude = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -100,6 +134,26 @@ class SelectCityFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btSearch.setOnClickListener(::onMapSearchButtonClick)
+
+        //位置情報取得関連
+        //位置情報取得クライアントを作成
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 5000
+        ).build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                //lastLocation: 最新の位置情報
+                result.lastLocation?.also { location ->
+                    //緯度経度取る
+                    latitude = location.latitude
+                    longitude = location.longitude
+                }
+            }
+        }
+
         binding.rvCityList.apply {
             //CityAdapterクラスの新しいインスタンスを作成、その際に都市データのリスト(CityクラスのcityList)を渡している
             adapter = CityAdapter(cityList){
@@ -113,6 +167,49 @@ class SelectCityFragment : Fragment() {
             //区切り線が追加されて見やすくなるだけ
             addItemDecoration(DividerItemDecoration(context, manager.orientation))
         }
+    }
+
+    //取得開始のリクエストを投げる↓
+    override fun onResume() {
+        super.onResume()
+        requestLocationUpdates()
+    }
+
+    //リソースの開放
+    override fun onPause() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        super.onPause()
+    }
+
+    @UiThread
+    private fun onMapSearchButtonClick(view: View) {
+        lifecycleScope.launch {
+
+            val locationUrl = "$WEATHER_INFO_URL&lat=$latitude&lon=$longitude&appid=$APP_ID"
+
+            val result = ktorClient.get {
+                url(locationUrl)
+            }.body<WeatherInfo>()
+
+            parentFragmentManager.setFragmentResult(
+                //bundleOf() -> キーがRESULT_NAME, 値がit.nameのBundleオブジェクトを作成
+                REQUEST_SELECTED_CITY, bundleOf(
+                    GAINED_CITY_NAME to result.cityInfo.cityName,
+                    GAINED_CITY_DATETEXT to result.hourlyList[0].dateText,
+                    GAINED_CITY_WEATHER_DESC to result.hourlyList[0].weather[0].description,
+                    GAINED_TEMP to result.hourlyList[0].mainContents.temperature -273,
+                    GAINED_FEELS_LIKE to result.hourlyList[0].mainContents.feelsLike -273,
+                    GAINED_PRESS to result.hourlyList[0].mainContents.pressure,
+                    GAINED_HUMIDITY to result.hourlyList[0].mainContents.humidity,
+                    GAINED_POP to result.hourlyList[0].rainPer*100,
+                    GAINED_WIND_SPEED to result.hourlyList[0].wind.speed,
+                    GAINED_WIND_MAX to result.hourlyList[0].wind.gust,
+                    GAINED_WIND_DEG to result.hourlyList[0].wind.windDegrees
+                )
+            )
+
+        }
+
     }
 
     @UiThread
@@ -150,23 +247,6 @@ class SelectCityFragment : Fragment() {
             Log.d("TAG", "DateText: ${result.hourlyList[0].dateText}")
 
 
-
-
-            // 取得したデータを UI に反映
-//            result.run {
-//                binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityInfo.cityName)
-//                binding.tvWeatherDesc.text = getString(R.string.tv_desc,hourlyList[0].dateText ,hourlyList[0].weather[0].description)
-//                binding.tvWetherMore.text = getString(
-//                    R.string.tv_more,
-//                    hourlyList[0].mainContents.temperature -273,
-//                    hourlyList[0].mainContents.feelsLike -273,
-//                    hourlyList[0].mainContents.pressure,
-//                    hourlyList[0].mainContents.humidity,
-//                    hourlyList[0].rainPer,
-//                    hourlyList[0].wind.speed,
-//                    hourlyList[0].wind.gust,
-//                    hourlyList[0].wind.windDegrees
-//                )
 //
 //                // 天気アイコンのURL
 //                val iconUrl = "$IMAGE_URL${hourlyList[0].weather[0].icon}$IMAGE_FORMAT"
@@ -177,6 +257,36 @@ class SelectCityFragment : Fragment() {
 //                    .into(binding.ivWeatherIcon)
 //            }
         }
+    }
+
+    private fun requestLocationUpdates(){
+        //true or false
+        val isGrantedFineLocation = ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //true or false
+        val isGrantedCoarseLocation = ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //位置情報取得の権限↑のうち、どちらか一方でも権限あれば(=trueなら)OKなので位置情報取得開始
+        if (isGrantedFineLocation || isGrantedCoarseLocation){
+            //権限チェック処理入れないと赤線が消えない
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                requireActivity().mainLooper
+            )
+            return
+        }
+        //ここまで来たらどの権限も無いのでリクエストm
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
 
